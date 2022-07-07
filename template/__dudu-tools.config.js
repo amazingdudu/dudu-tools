00c1fd625e28fd85aaa789380b806283d9472667
shaoyangyang <yangyang.shao@brgroup.com>
2022-07-07 17:11:25 +0800

    feat: 支持自定义postcss配置

---
+++ template/__dudu-tools.config.js
@@ -0,0 +1,13 @@
+/**
+ *  const autoprefixer = require('autoprefixer');
+ *  const pxtorem = require('postcss-pxtorem');
+ * 
+ *  module.exports = {
+ *    postcss: [autoprefixer(), pxtorem({})]
+ *  }
+ * 
+ */
+
+module.exports = {
+    postcss: []
+}
